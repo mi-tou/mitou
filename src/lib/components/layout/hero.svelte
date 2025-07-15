f793b5044cc6a7806865acbc479562bfae7b3634
wenke.lai <wenke.studio@gmail.com>
2025-07-15 11:39:59 +0800

    feat(layout): add hero section

---
+++ src/lib/components/layout/hero.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+    interface HeroProps {
+        title: string;
+        description?: string;
+    }
+</script>
+
+<script lang="ts">
+    const { title, description }: HeroProps = $props();
+</script>
+
+<section class="hero relative h-96">
+    <div class="prose xl:prose-xl p-8 text-center">
+        <h2 class="capitalize">{title}</h2>
+        <p>{description}</p>
+    </div>
+</section>
