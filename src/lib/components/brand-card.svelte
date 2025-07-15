1bb37b1106d59849ad4111658af2b5440e3ca381
wenke.lai <wenke.studio@gmail.com>
2025-07-15 23:54:25 +0800

    fix(brand): enhance card style

--- src/lib/components/brand-card.svelte
+++ src/lib/components/brand-card.svelte
@@ -10,16 +10,23 @@
     const { brand }: BrandCardProps = $props();
 </script>
 
-<div class="card hover:-translate-y-2">
+<a class="card card-border card-lg hover:-translate-y-2" href="/brand/{brand.id}">
     <figure>
-        <img src={brand.image} alt={brand.name} class="h-64" />
+        <img
+            src={brand.image}
+            alt={brand.name}
+            class="size-full h-64 object-contain object-center md:object-cover"
+        />
     </figure>
     <div class="card-body">
         <div class="card-title">
             <h3>{brand.name}</h3>
+        </div>
+        <div class="flex items-center gap-2">
+            <h3>{brand.owner}</h3>
             <div class="badge badge-secondary">{brand.country}</div>
         </div>
-        <h3>品牌所屬: {brand.owner}</h3>
-        <div class="card-actions justify-end"></div>
+
+        <div class="card-actions"></div>
     </div>
-</div>
+</a>
