338feb417ce099a8a4846cb4ae68904429ad4574
wenke.lai <wenke.studio@gmail.com>
2025-07-15 23:54:09 +0800

    feat(data): add channels for brands

--- src/lib/data/brands.ts
+++ src/lib/data/brands.ts
@@ -1,3 +1,8 @@
+export type Channel = {
+    name: string;
+    href: string;
+};
+
 export type Brand = {
     id: string;
     name: string;
@@ -5,6 +10,7 @@ export type Brand = {
     owner: string;
     country: string;
     image: string;
+    channels: Channel[];
 };
 
 export const brands: Brand[] = [
@@ -12,33 +18,86 @@ export const brands: Brand[] = [
         id: "pampers",
         name: "幫寶適",
         href: "https://pampers.com.tw/",
-        owner: "Procter & Gamble",
+        owner: "Procter & Gamble (寶僑)",
         country: "美國",
-        image: "/image/brand/pampers.png"
+        image: "/image/brand/pampers.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400054&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO09" },
+            { name: "costco", href: "https://www.costco.com.tw/Pampers/c/Pampers" },
+            {
+                name: "大樹藥局",
+                href: "https://shop.greattree.com.tw/campaign?id=b67f45aa-c8ee-4446-bfc2-e77bdd1bfc10"
+            }
+        ]
     },
     {
         id: "mamypoko",
         name: "滿意寶寶",
         href: "https://www.mamypoko.com.tw/",
-        owner: "Unicharm",
+        owner: "ユニ·チャーム株式会社 (嬌聯)",
         country: "日本",
-        image: "/image/brand/mamypoko.png"
+        image: "/image/brand/mamypoko.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400049&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO00" },
+            {
+                name: "蝦皮購物",
+                href: "https://shopee.tw/unicharm_taiwan"
+            },
+            {
+                name: "大樹藥局",
+                href: "https://shop.greattree.com.tw/productlist?item1=01&item2=0199&item3=019914"
+            },
+            {
+                name: "丁丁藥局",
+                href: "https://www.norbelbaby.com.tw/nec/search?keyword=%E6%BB%BF%E6%84%8F%E5%AF%B6%E5%AF%B6"
+            }
+        ]
     },
     {
         id: "huggies",
         name: "好奇",
         href: "https://www.huggies.com.tw/",
-        owner: "Kimberly Clark",
-        country: "日本",
-        image: "/image/brand/huggies.png"
+        owner: "Kimberly-Clark Corporation",
+        country: "美國",
+        image: "/image/brand/huggies.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400108&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAOAL" },
+            {
+                name: "costco",
+                href: "https://www.costco.com.tw/Household-Baby-Toys/Baby/Baby-Diapers-Wipes/c/130501?q=:relevance:Brand:Huggies"
+            }
+        ]
     },
     {
         id: "libero",
         name: "麗貝樂",
         href: "https://www.libero.com/",
-        owner: "維達",
-        country: "中國",
-        image: "/image/brand/libero.png"
+        owner: "Essity",
+        country: "瑞典",
+        image: "/image/brand/libero.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400508&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO1R" },
+            {
+                name: "costco",
+                href: "https://www.costco.com.tw/Household-Baby-Toys/Baby/Baby-Diapers-Wipes/c/130501?q=:relevance:Brand:Libero"
+            }
+        ]
     },
     {
         id: "merries",
@@ -46,7 +105,22 @@ export const brands: Brand[] = [
         href: "https://www.kao.com/tw/products/merries/",
         owner: "花王株式会社",
         country: "日本",
-        image: "/image/brand/merries.png"
+        image: "/image/brand/merries.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400058&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO3B" },
+            {
+                name: "costco",
+                href: "https://www.costco.com.tw/Merries/c/Merries"
+            },
+            {
+                name: "大樹藥局",
+                href: "https://shop.greattree.com.tw/campaign?id=092d4dd6-51e3-4078-85a9-90e11947107e"
+            }
+        ]
     },
     {
         id: "carnation",
@@ -54,7 +128,8 @@ export const brands: Brand[] = [
         href: "https://www.knh.com.tw/product-detail-2153046.html",
         owner: "康那香企業股份有限公司",
         country: "台灣",
-        image: "/image/brand/carnation.png"
+        image: "/image/brand/carnation.png",
+        channels: []
     },
     {
         id: "purebaby",
@@ -62,7 +137,8 @@ export const brands: Brand[] = [
         href: "https://www.yome.in/",
         owner: "釉暉國際有限公司",
         country: "台灣",
-        image: "/image/brand/purebaby.png"
+        image: "/image/brand/purebaby.png",
+        channels: []
     },
     {
         id: "noopii",
@@ -70,15 +146,28 @@ export const brands: Brand[] = [
         href: "https://www.goodgoods.design/",
         owner: "拾物工作室",
         country: "台灣",
-        image: "/image/brand/noopii.png"
+        image: "/image/brand/noopii.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400751&sourcePageType=4"
+            }
+        ]
     },
     {
         id: "goo.n",
-        name: "大王",
+        name: "日本大王",
         href: "https://www.goondiaper.com/",
         owner: "大王製紙株式會社",
         country: "日本",
-        image: "/image/brand/goo.n.png"
+        image: "/image/brand/goo.n.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400062&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO0F" }
+        ]
     },
     {
         id: "nepia-genki",
@@ -86,6 +175,13 @@ export const brands: Brand[] = [
         href: "https://cdn.e-nepia.com/genki/pc/product/overseas.html",
         owner: "王子製紙株式会社",
         country: "日本",
-        image: "/image/brand/nepia-genki.png"
+        image: "/image/brand/nepia-genki.png",
+        channels: [
+            {
+                name: "momo購物網",
+                href: "https://www.momoshop.com.tw/category/MgrpCategory.jsp?m_code=2700400610&sourcePageType=4"
+            },
+            { name: "pchome 24h購物", href: "https://24h.pchome.com.tw/store/DAAO54" }
+        ]
     }
 ];
